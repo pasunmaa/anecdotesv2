be0d6d27e0140cb10fdcbe784a81fe72f27a8dc8
Petri Asunmaa <petrieas@gmail.com>
2018-08-11 16:54:12 +0300

    Tehtävä 6.16 valmis

--- routed/src/App.js
+++ routed/src/App.js
@@ -1,19 +1,20 @@
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import React from 'react'
 
-const Menu = () => (
-  <div>    
+/* const Menu = () => (
+  <div>
     <a href='#'>anecdotes</a>&nbsp;
     <a href='#'>create new</a>&nbsp;
     <a href='#'>about</a>&nbsp;
   </div>
-)
+) */
 
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
       {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
-    </ul>  
+    </ul>
   </div>
 )
 
@@ -21,10 +22,10 @@ const About = () => (
   <div>
     <h2>About anecdote app</h2>
     <p>According to Wikipedia:</p>
-    
-    <em>An anecdote is a brief, revealing account of an individual person or an incident. 
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself, 
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative. 
+
+    <em>An anecdote is a brief, revealing account of an individual person or an incident.
+      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
+      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
       An anecdote is "a story with a point."</em>
 
     <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
@@ -35,7 +36,8 @@ const Footer = () => (
   <div>
     Anecdote app for <a href='https://courses.helsinki.fi/fi/TKT21009/121540749'>Full Stack -sovelluskehitys</a>.
 
-    See <a href='https://github.com/mluukkai/routed-anecdotes'>https://github.com/mluukkai/routed-anecdotes</a> for the source code. 
+    See <a href='https://github.com/pasunmaa/anecdotesv2/tree/master/routed'>
+      https://github.com/pasunmaa/anecdotesv2/tree/master/routed</a> for the source code.
   </div>
 )
 
@@ -70,7 +72,7 @@ class CreateNew extends React.Component {
         <h2>create a new anecdote</h2>
         <form onSubmit={this.handleSubmit}>
           <div>
-            content 
+            content
             <input name='content' value={this.state.content} onChange={this.handleChange} />
           </div>
           <div>
@@ -80,10 +82,10 @@ class CreateNew extends React.Component {
           <div>
             url for more info
             <input name='info' value={this.state.info} onChange={this.handleChange} />
-          </div> 
+          </div>
           <button>create</button>
         </form>
-      </div>  
+      </div>
     )
 
   }
@@ -111,7 +113,7 @@ class App extends React.Component {
         }
       ],
       notification: ''
-    } 
+    }
   }
 
   addNew = (anecdote) => {
@@ -139,14 +141,23 @@ class App extends React.Component {
     return (
       <div>
         <h1>Software anecdotes</h1>
-          <Menu />
-          <AnecdoteList anecdotes={this.state.anecdotes} />
-          <About />      
-          <CreateNew addNew={this.addNew}/>
+        <Router>
+          <div>
+            <div>
+              <Link to="/">anecdotes</Link> &nbsp;
+              <Link to="/create">create new</Link> &nbsp;
+              <Link to="/about">about</Link>
+            </div>
+            <Route exact path="/" render={() =>
+              <AnecdoteList anecdotes={this.state.anecdotes} />} />
+            <Route path="/create" render={() => <CreateNew addNew={this.addNew} />} />
+            <Route path="/about" render={() => <About />} />
+          </div>
+        </Router>
         <Footer />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
