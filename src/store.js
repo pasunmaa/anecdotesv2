a44afd2e78872af354cb3c8ee6e6f19b03f7025f
Petri Asunmaa <petrieas@gmail.com>
2018-08-11 12:34:44 +0300

    Redux devtools enabled

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import filterReducer from './reducers/filterReducer'
 import notificationReducer from './reducers/notificationReducer'
 import anecdoteReducer from './reducers/anecdoteReducer'
@@ -13,7 +14,9 @@ const reducer = combineReducers({
 
 const store = createStore(
   reducer,
-  applyMiddleware(thunk)
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
 )
 
 export default store
