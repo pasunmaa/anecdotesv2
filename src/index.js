5f9184b6508d12331eeed53f49a65e314c9cd801
Petri Asunmaa <petrieas@gmail.com>
2018-08-11 09:33:29 +0300

    6.10 getAll in Index

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import anecdoteService from './services/anecdotes'
 import App from './App'
 import filterReducer from './reducers/filterReducer'
 import notificationReducer from './reducers/notificationReducer'
-import anecdoteReducer from './reducers/anecdoteReducer'
+import anecdoteReducer, { anecdoteInitialization } from './reducers/anecdoteReducer'
 
 const reducer = combineReducers({
   filter: filterReducer,
@@ -14,17 +15,15 @@ const reducer = combineReducers({
 })
 
 const store = createStore(reducer)
-
 //console.log(store.getState())
 
-//const render = () => {
+anecdoteService.getAll().then(notes =>
+  store.dispatch(anecdoteInitialization(notes))
+)
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
 )
-//}
-
-/* render()
-store.subscribe(render) */
