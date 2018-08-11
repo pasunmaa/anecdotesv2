ff8c84110d798da7b8ca006ac6a9e1bcf8c22273
Petri Asunmaa <petrieas@gmail.com>
2018-08-11 08:48:41 +0300

    Tehtävä 6.9 valmis

--- src/components/AnecdoteList.js
+++ src/components/AnecdoteList.js
@@ -5,53 +5,47 @@ import { notificationSet, notificationReset } from '../reducers/notificationRedu
 import { connect } from 'react-redux'
 
 
-class AnecdoteList extends React.Component {
-  vote = (id) => () => {
-    const anecdotes = this.props.anecdotes
-    this.props.anecdoteVoting(id)
-    this.props.notificationSet(
-      'You have voted \''+anecdotes.find(a => a.id === id ).content+'\'')
-    window.setTimeout(() => this.props.notificationReset(), 5000)
+const AnecdoteList = (props) => {
+  const vote = (id) => () => {
+    props.anecdoteVoting(id)
+    props.notificationSet(
+      'You have voted \''+props.filteredAnecdotes.find(a => a.id === id ).content+'\'')
+    window.setTimeout(() => props.notificationReset(), 5000)
   }
 
-  filteredAnecdotes = (filter) => {
-    if (filter.length)
-      return this.props.anecdotes.filter(
-        a => a.content.toLowerCase().indexOf(filter) !== -1)
-    else
-      return this.props.anecdotes
-  }
-
-  render() {
-    const anecdotes = this.filteredAnecdotes(this.props.filter)
-    return (
-      <div>
-        <h2>Anecdotes</h2>
-        <Filter />
-        {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
-          <div key={anecdote.id}>
-            <div>
-              {anecdote.content}
-            </div>
-            <div>
-              has {anecdote.votes} {'  '}
-              <button onClick={ this.vote(anecdote.id) }>
-                vote
-              </button>
-            </div>
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <Filter />
+      {props.filteredAnecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
           </div>
-        )}
-      </div>
-    )
-  }
+          <div>
+              has {anecdote.votes} {'  '}
+            <button onClick={ vote(anecdote.id) }>
+                vote
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+const filteredAnecdotes = (anecdotes, filter) => {
+  if (filter.length)
+    return anecdotes.filter(
+      a => a.content.toLowerCase().indexOf(filter) !== -1)
+  else
+    return anecdotes
 }
 
 const mapStateToProps = (state) => {
   //console.log(state)
   return {
-    filter: state.filter,
-    //notification: state.notification,
-    anecdotes: state.anecdotes
+    filteredAnecdotes: filteredAnecdotes(state.anecdotes, state.filter)
   }
 }
 
