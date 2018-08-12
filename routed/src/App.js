132af7b6551188be252ec5f5477680b6326e4f13
Petri Asunmaa <petrieas@gmail.com>
2018-08-12 10:41:32 +0300

    Tehtävä 6.22 valmis

--- routed/src/App.js
+++ routed/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
-import { ListGroup, ListGroupItem } from 'react-bootstrap'
+import { ListGroup, ListGroupItem, Grid, Row, Col } from 'react-bootstrap'
 
 const Navi = () => {
   const menuStyle = {
@@ -70,14 +70,25 @@ const Show = ({ anecdote }) => (
 const About = () => (
   <div>
     <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
+    <Grid>
+      <Row className="show-grid">
+        <Col xs={12} md={8}>
+          <p>According to Wikipedia:</p>
 
-    <em>An anecdote is a brief, revealing account of an individual person or an incident.
+          <em>An anecdote is a brief, revealing account of an individual person or an incident.
       Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
       such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
       An anecdote is &quota story with a point.&quot</em>
 
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+          <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+        </Col>
+        <Col xs={6} md={4}>
+          <img src="https://i.pinimg.com/564x/34/1d/99/341d9919bc749b2c52a3a8e64190363b.jpg"
+            width="40%" height="40%"
+            alt="Linux Torvald" />
+        </Col>
+      </Row>
+    </Grid>
   </div>
 )
 
